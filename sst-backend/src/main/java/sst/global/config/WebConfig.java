eb4fc45eb42edfc1fc887e7de6fe1f8ba0d7a63c
koreasmart <sugang@ksmart.or.kr>
2026-05-19 13:54:15 +0900

    로그 설정

--- sst-backend/src/main/java/sst/global/config/WebConfig.java
+++ sst-backend/src/main/java/sst/global/config/WebConfig.java
@@ -1,19 +1,32 @@
 package sst.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import lombok.RequiredArgsConstructor;
+import sst.global.interceptor.LogInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
 	private final FileStorageConfig config;
+	private final LogInterceptor logInterceptor;
 	
-    @Override
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(logInterceptor)
+		.addPathPatterns("/**")
+		.excludePathPatterns(
+				"/error",        // 에러 페이지
+				"/favicon.ico"   // 브라우저 아이콘 요청
+		);
+	}
+
+	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry
@@ -23,4 +36,6 @@ public class WebConfig implements WebMvcConfigurer {
 			.resourceChain(true)
 			.addResolver(new PathResourceResolver());
     }
+    
+
 }
